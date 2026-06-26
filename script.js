f4fef63c3b8e51661577cf178e12104b2ba2c2a2
uaimarquez <estudosmarquez@outlook.com>
2026-06-26 05:45:46 -0300

    ultima commit eu juro

--- script.js
+++ script.js
@@ -148,4 +148,13 @@ function renderizarMisterios() {
     container.innerHTML = html;
 }
 
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
+
 renderizarMisterios();
+
+
